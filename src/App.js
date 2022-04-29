bc57836908b090f983854d9649ecdb7eb5264a42
AshkanAhmadi <ashkan16ma96@gmail.com>
2022-04-29 18:58:30 +0430

    add Todos App & design with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Provider } from "react-redux";
 import Counter from "./Components/Counter";
 import { store } from "./features/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddTodoForm from "./Components/Todos/AddTodoForm";
+import TodoList from "./Components/Todos/TodoList";
+import TotalCompleteItems from "./Components/Todos/TotalCompleteItems";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Counter />
+        <AddTodoForm />
+        <TodoList />
+        <TotalCompleteItems />
       </div>
     </Provider>
   );
